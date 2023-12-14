7cc80035d83a0cac3dc57d87ff69a822a1db93da
L1Z4Q <L1Z4Q@DESKTOP-1IVDGGM>
2023-12-14 16:41:44 +0100

    Add project files.

---
+++ Projekt/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Projekt
+{
+    public partial class Form1 : Form
+    {
+        private SortAlgorithm sorter;
+        private List<int> unsortedData;
+        private Dictionary<string, long> algorithmTimes;
+
+        private System.Windows.Forms.ListBox listBox1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            sorter = new SortAlgorithm();
+            unsortedData = new List<int>();
+            algorithmTimes = new Dictionary<string, long>();
+
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(538, 350);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(300, 95);
+            this.listBox1.TabIndex = 13;
+
+            this.Controls.Add(this.listBox1);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            GenerateData();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            GenerateData();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            GenerateData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateChart();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                GenerateData();
+            }
+            else
+            {
+                chart1.Series.Clear();
+                unsortedData.Clear();
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void chart2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox1.Text, out int dataSize) || dataSize <= 0)
+            {
+                MessageBox.Show("Wprowadź poprawną ilość elementów do posortowania.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            SortAndDisplay();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Wyczyść wybór rodzaju ciągu do sortowania
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+
+            // Wyczyść wybór rodzaju algorytmu do sortowania
+            comboBox1.SelectedIndex = -1;
+
+            // Wyczyść pole ilości elementów do sortowania
+            textBox1.Clear();
+
+            // Wyczyść obie wykresy
+            chart1.Series.Clear();
+            chart2.Series.Clear();
+
+            // Wyczyść listę czasów algorytmów
+            algorithmTimes.Clear();
+
+            // Wyczyść listbox z czasami
+            listBox1.Items.Clear();
+
+            // Aktualizuj etykietę z czasem sortowania
+            label5.Text = "label5";
+        }
+
+        private void GenerateData()
+        {
+            int dataSize;
+            if (int.TryParse(textBox1.Text, out dataSize))
+            {
+                if (radioButton1.Checked)
+                    unsortedData = GenerateAscendingData(dataSize);
+                else if (radioButton2.Checked)
+                    unsortedData = GenerateDescendingData(dataSize);
+                else
+                    unsortedData = GenerateRandomData(dataSize);
+            }
+
+            UpdateChart();
+        }
+
+        private List<int> GenerateAscendingData(int size)
+        {
+            List<int> data = new List<int>();
+            for (int i = 1; i <= size; i++)
+            {
+                data.Add(i);
+            }
+            return data;
+        }
+
+        private List<int> GenerateDescendingData(int size)
+        {
+            List<int> data = new List<int>();
+            for (int i = size; i > 0; i--)
+            {
+                data.Add(i);
+            }
+            return data;
+        }
+
+        private List<int> GenerateRandomData(int size)
+        {
+            List<int> data = new List<int>();
+            Random random = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                data.Add(random.Next(1, 100));
+            }
+            return data;
+        }
+
+        private void UpdateChart()
+        {
+            chart1.Series.Clear();
+            chart1.Series.Add("Unsorted");
+            chart1.Series["Unsorted"].Points.DataBindY(unsortedData);
+        }
+
+        private void SortAndDisplay()
+        {
+            string selectedAlgorithm = comboBox1.SelectedItem?.ToString();
+
+            if (string.IsNullOrEmpty(selectedAlgorithm))
+            {
+                MessageBox.Show("Wybierz algorytm sortowania.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            List<int> sortedData = new List<int>(unsortedData);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            switch (selectedAlgorithm)
+            {
+                case "Sortowanie bąbelkowe":
+                    sorter.BubbleSort(sortedData);
+                    break;
+
+                case "Sortowanie przez wybór":
+                    sorter.SelectionSort(sortedData);
+                    break;
+
+                case "Sortowanie przez wstawianie":
+                    sorter.InsertionSort(sortedData);
+                    break;
+
+                case "Sortowanie szybkie":
+                    sorter.QuickSort(sortedData);
+                    break;
+
+                case "Sortowanie przez scalanie":
+                    sortedData = sorter.MergeSort(sortedData);
+                    break;
+
+                default:
+                    break;
+            }
+
+            stopwatch.Stop();
+
+            algorithmTimes[selectedAlgorithm] = stopwatch.ElapsedTicks;
+
+            if (chart1.Series.IndexOf("Sorted") != -1)
+            {
+                chart1.Series.Remove(chart1.Series["Sorted"]);
+            }
+
+            Series sortedSeries = new Series("Sorted");
+            sortedSeries.Points.DataBindY(sortedData);
+            chart1.Series.Add(sortedSeries);
+
+            label5.Text = $"Czas sortowania ({selectedAlgorithm}): {stopwatch.ElapsedTicks} ticks";
+
+            UpdateAlgorithmTimeChart();
+        }
+
+        private void UpdateAlgorithmTimeChart()
+        {
+            chart2.Series.Clear();
+            listBox1.Items.Clear();
+
+            foreach (var kvp in algorithmTimes)
+            {
+                listBox1.Items.Add($"Czas sortowania ({kvp.Key}): {kvp.Value} ticks");
+
+                chart2.Series.Add(kvp.Key);
+                chart2.Series[kvp.Key].Points.AddY(kvp.Value);
+            }
+        }
+    }
+}
---
+++ Projekt/Sortowania.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections.Generic;
+
+public class SortAlgorithm
+{
+    public void BubbleSort(List<int> data)
+    {
+        int n = data.Count;
+        bool swapped;
+        for (int i = 0; i < n - 1; i++)
+        {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                if (data[j] > data[j + 1])
+                {
+                    // Zamiana miejscami
+                    int temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // Jeśli nie było zamiany w danej iteracji, to zakończ sortowanie
+            if (!swapped)
+                break;
+        }
+    }
+
+    public void SelectionSort(List<int> data)
+    {
+        int n = data.Count;
+        for (int i = 0; i < n - 1; i++)
+        {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (data[j] < data[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i)
+            {
+                // Zamiana miejscami tylko jeśli znajdziemy nowy minimum
+                int temp = data[minIndex];
+                data[minIndex] = data[i];
+                data[i] = temp;
+            }
+        }
+    }
+
+    public void InsertionSort(List<int> data)
+    {
+        int n = data.Count;
+        for (int i = 1; i < n; i++)
+        {
+            int key = data[i];
+            int j = i - 1;
+
+            // Przesuwanie elementów większych od key
+            while (j >= 0 && data[j] > key)
+            {
+                data[j + 1] = data[j];
+                j = j - 1;
+            }
+            data[j + 1] = key;
+        }
+    }
+
+    public void QuickSort(List<int> data)
+    {
+        QuickSort(data, 0, data.Count - 1);
+    }
+
+    private void QuickSort(List<int> data, int low, int high)
+    {
+        if (low < high)
+        {
+            int pi = Partition(data, low, high);
+
+            QuickSort(data, low, pi);
+            QuickSort(data, pi + 1, high);
+        }
+    }
+
+    private int Partition(List<int> data, int low, int high)
+    {
+        int pivot = data[low];
+        int i = low - 1;
+        int j = high + 1;
+
+        while (true)
+        {
+            do
+            {
+                i++;
+            } while (data[i] < pivot);
+
+            do
+            {
+                j--;
+            } while (data[j] > pivot);
+
+            if (i >= j)
+                return j;
+
+            // Zamiana miejscami
+            int temp = data[i];
+            data[i] = data[j];
+            data[j] = temp;
+        }
+    }
+
+    public List<int> MergeSort(List<int> data)
+    {
+        if (data.Count <= 1)
+            return data;
+
+        int middle = data.Count / 2;
+        List<int> left = new List<int>(data.GetRange(0, middle));
+        List<int> right = new List<int>(data.GetRange(middle, data.Count - middle));
+
+        left = MergeSort(left);
+        right = MergeSort(right);
+
+        return Merge(left, right);
+    }
+
+    private List<int> Merge(List<int> left, List<int> right)
+    {
+        List<int> result = new List<int>();
+        int i = 0, j = 0;
+
+        while (i < left.Count && j < right.Count)
+        {
+            if (left[i] < right[j])
+            {
+                result.Add(left[i]);
+                i++;
+            }
+            else
+            {
+                result.Add(right[j]);
+                j++;
+            }
+        }
+
+        while (i < left.Count)
+        {
+            result.Add(left[i]);
+            i++;
+        }
+
+        while (j < right.Count)
+        {
+            result.Add(right[j]);
+            j++;
+        }
+
+        return result;
+    }
+}
